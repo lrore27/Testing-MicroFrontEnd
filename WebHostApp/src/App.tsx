eead2a5665ad1b1164b706c9e4a5e6cf7297885e
Lorenzo Tricarico <99330195+lrore27@users.noreply.github.com>
2022-11-06 02:05:01 +0100

    Init WebHostApp with module federation

---
+++ WebHostApp/src/App.tsx
@@ -0,0 +1,16 @@
+// @ts-ignore
+import React from "react";
+// @ts-ignore
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <div>Name: WebHostApp</div>
+    <div>Framework: react</div>
+    <div>Language: TypeScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
