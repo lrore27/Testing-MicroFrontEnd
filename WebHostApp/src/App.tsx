a8f05673c0ba13aa365d7d04d18d0ef72696a078
Lorenzo Tricarico <99330195+lrore27@users.noreply.github.com>
2022-11-07 18:22:55 +0100

    Init LibrariesApp with module federation

--- WebHostApp/src/App.tsx
+++ WebHostApp/src/App.tsx
@@ -5,12 +5,15 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-const App = () => (
-  <div className="container">
-    <div>Name: WebHostApp</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
-  </div>
-);
-ReactDOM.render(<App />, document.getElementById("app"));
+const App = () => {
+    return (
+        <div className="container">
+            <div>Name: WebHostApp</div>
+            <div>Framework: react</div>
+            <div>Language: TypeScript</div>
+            <div>CSS: Empty CSS</div>
+        </div>
+    );
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"));
